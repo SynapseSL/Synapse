7aba04cf6c552fb8a8bbc08970dac96051d9b424
GrafDimenzio <crafterwolfhd@gmail.com>
2020-09-12 10:08:55 +0200

    added gameobject.getplayer and Player objects

--- Synapse/Api/Player.cs
+++ Synapse/Api/Player.cs
@@ -1,17 +1,86 @@
-﻿using UnityEngine;
+﻿using Hints;
+using RemoteAdmin;
+using Searching;
+using UnityEngine;
 
 namespace Synapse.Api
 {
     public class Player : MonoBehaviour
     {
+        internal Player()
+        {
+            Hub = GetComponent<ReferenceHub>();
+        }
+
+        #region Default Stuff
+        public string NickName 
+        { 
+            get => NicknameSync.Network_myNickSync;
+        }
+
+        public string DisplayName 
+        { 
+            get => NicknameSync.DisplayName; 
+            set => NicknameSync.DisplayName = value; 
+        }
+
+        public int PlayerId
+        {
+            get => QueryProcessor.PlayerId;
+            set => QueryProcessor.NetworkPlayerId = value;
+        }
+
         public string UserId
         {
             get => ClassManager.UserId;
             set => ClassManager.UserId = value;
         }
 
+        public string SecondUserID
+        {
+            get => ClassManager.UserId2;
+            set => ClassManager.UserId2 = value;
+        }
+
+        public string IpAddress => QueryProcessor._ipAddress;
+        #endregion
+
+        #region ReferenceHub
+        public Transform CameraReference => Hub.PlayerCameraReference;
+
+        public HintDisplay HintDisplay => Hub.hints;
+
+        public SearchCoordinator SearchCoordinator => Hub.searchCoordinator;
+
+        public FootstepSync FootstepSync => Hub.footstepSync;
+
+        public PlayerEffectsController PlayerEffectsController => Hub.playerEffectsController;
+
+        public PlayerInteract PlayerInteract => Hub.playerInteract;
+
+        public Handcuffs Handcuffs => Hub.handcuffs;
+
+        public FallDamage FallDamage => Hub.falldamage;
+
+        public AnimationController AnimationController => Hub.animationController;
+
+        public SpectatorManager SpectatorManager => Hub.spectatorManager;
+
+        public NicknameSync NicknameSync => Hub.nicknameSync;
+
+        public PlayerMovementSync PlayerMovementSync => Hub.playerMovementSync;
+
+        public QueryProcessor QueryProcessor => Hub.queryProcessor;
+
+        public ServerRoles ServerRoles => Hub.serverRoles;
+
+        public PlayerStats PlayerStats => Hub.playerStats;
+
+        public Inventory Inventory => Hub.inventory;
+
         public CharacterClassManager ClassManager => Hub.characterClassManager;
         
-        public ReferenceHub Hub => GetComponent<ReferenceHub>();
+        public ReferenceHub Hub { get; internal set; }
+        #endregion
     }
 }
--- Synapse/SynapseExtensions.cs
+++ Synapse/SynapseExtensions.cs
@@ -4,4 +4,6 @@ using UnityEngine;
 public static class SynapseExtensions
 {
     public static Player GetPlayer(this MonoBehaviour mono) => mono.gameObject.GetComponent<Player>();
+
+    public static Player GetPlayer(this GameObject gameObject) => gameObject.GetComponent<Player>();
 }
