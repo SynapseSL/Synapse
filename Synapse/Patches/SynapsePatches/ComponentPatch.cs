b524368001bf6dafca1c9ed731f8abcd663e8b9d
Robin Möller <moelrobi@gmail.com>
2020-09-12 14:07:30 +0200

    Fix PlayerJoin and add ComponentPatch

---
+++ Synapse/Patches/SynapsePatches/ComponentPatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using GameCore;
+using Harmony;
+using Synapse.Api;
+
+namespace Synapse.Patches.SynapsePatches
+{
+    [HarmonyPatch(typeof(ReferenceHub), nameof(ReferenceHub.LoadComponents))]
+    public static class ComponentsPatch
+    {
+        public static void Prefix(ReferenceHub __instance)
+        {
+            if (__instance.GetComponent<Player>() == null) __instance.gameObject.AddComponent<Player>();
+
+            try
+            {
+                //TODO: Events.InvokeLoadComponents(__instance.gameObject);
+            }
+            catch (Exception e)
+            {
+                SynapseController.Server.Logger.Error($"Synapse-Event: LoadComponents failed!!\n{e}");
+            }
+        }
+    }
+}
