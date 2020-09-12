c95f4a48aa2a7ad046a7318f409a2cff93b4206a
GrafDimenzio <crafterwolfhd@gmail.com>
2020-09-12 11:57:25 +0200

    Revert "Helight Dev Changes"
    This reverts commit e08fdaeabf74bcc44d318b4be5f89235c98fcf3d.

--- SynapseInjector/Loader.cs
+++ SynapseInjector/Loader.cs
@@ -1,63 +1,10 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using UnityEngine;
-
-namespace SynapseInjector
+﻿namespace SynapseInjector
 {
     public class Loader
     {
-        /// <summary>
-        /// Entrypoint for Synapse
-        /// </summary>
-        public static void LoadSystem()
+        public void LoadSystem()
         {
-            printBanner();
-
-            var synapse = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Synapse");
-            if (!Directory.Exists(synapse)) Directory.CreateDirectory(synapse);
-            InvokeAssembly(Path.Combine(synapse, "Synapse.dll"));
-
-        }
-
-        /// <summary>
-        /// Print Synapse Banner and Version Information
-        /// </summary>
-        private static void printBanner()
-        {
-            ServerConsole.AddLog(
-                "\nLoading Synapse...\n" +
-                "-----------------------------------------------\n" +
-                "  __                             \n" +
-                " (_       ._    _.  ._    _   _  \n" +
-                " __)  \\/  | |  (_|  |_)  _>  (/_ \n" +
-                "      /             |            \n" +
-                "\n" +
-                "LoaderVersion: " + Assembly.GetExecutingAssembly().GetName().Version + "\n" +
-                "RuntimeVersion: " + Assembly.GetExecutingAssembly().ImageRuntimeVersion + "\n" +
-                "-----------------------------------------------", ConsoleColor.Yellow);
-
-        }
-
-        /// <summary>
-        /// Scan assembly for synapse main class
-        /// and invoke the init method 
-        /// </summary>
-        /// <param name="path">The path of the assembly</param>
-        private static void InvokeAssembly(string path)
-        {
-            try
-            {
-                Assembly.Load(File.ReadAllBytes(path)).GetTypes()
-                    .First((Type t) => t.Name == "SynapseController").GetMethods()
-                    .First((MethodInfo m) => m.Name == "Init")
-                    .Invoke(null, null);
-            }
-            catch (Exception e)
-            {
-                Console.Error.WriteLine(e);
-            }
+            ServerConsole.AddLog("Test");
         }
     }
 }
