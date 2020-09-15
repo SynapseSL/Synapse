fe840a70bd6efd5768772dd14ca95fecc124a199
GrafDimenzio <crafterwolfhd@gmail.com>
2020-09-15 20:18:10 +0200

    started withTeslaEvent

---
+++ Synapse/Patches/EventsPatches/MapPatches/TriggerTeslaPatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Harmony;
+using System.Collections.Generic;
+using Synapse.Api;
+using System.Linq;
+using UnityEngine;
+using Logger = Synapse.Api.Logger;
+
+namespace Synapse.Patches.EventsPatches.MapPatches
+{
+    [HarmonyPatch(typeof(TeslaGate), nameof(TeslaGate.PlayersInRange))]
+    internal static class TriggerTeslaPatch
+    {
+        private static void Postfix(TeslaGate __instance, bool hurtRange, ref List<PlayerStats> __result)
+        {
+            try
+            {
+                __result = new List<PlayerStats>();
+                var Tesla = Map.Get.Teslas.FirstOrDefault(x => x.GameObject == __instance.gameObject);
+
+                foreach(var player in SynapseController.Server.Players)
+                {
+                    bool trigger = true;
+
+                    if (Vector3.Distance(Tesla.Position, player.Position) > Tesla.SizeOfTrigger || player.IsDead) 
+                        continue;
+
+                    //TODO: Invoke Tesla Event Parameter: player,Tesla,trigger,hurtRange
+
+                    if (trigger) __result.Add(player.PlayerStats);
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Get.Error($"Synapse-Event: TriggerTesla failed!!\n{e}");
+            }
+        }
+    }
+}
