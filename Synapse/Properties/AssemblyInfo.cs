50c8bd028f0afe7faf373d1fb8b0c1b604af04fd
Robin Möller <moelrobi@gmail.com>
2020-09-16 18:35:57 +0200

    + AssemblyInfo

---
+++ Synapse/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Synapse")]
+[assembly: AssemblyDescription("Der Synapse Loader")]
+[assembly: AssemblyCompany("Synapse-DevTeam")]
+[assembly: AssemblyProduct("Synapse")]
+[assembly: AssemblyCopyright("Copyright © Synapse-DevTeam 2020")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
