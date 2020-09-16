832a2608ecb3475876115cbdf179d963fda31703
Robin Möller <moelrobi@gmail.com>
2020-09-16 16:44:50 +0200

    + PreAuthentication

--- Synapse/Api/Events/ServerEvents.cs
+++ Synapse/Api/Events/ServerEvents.cs
@@ -1,7 +1,21 @@
-﻿namespace Synapse.Api.Events
+﻿using LiteNetLib;
+using Synapse.Api.Events.SynapseEventArguments;
+
+namespace Synapse.Api.Events
 {
     public class ServerEvents
     {
         internal ServerEvents() { }
+
+        public event EventHandler.OnSynapseEvent<PreAuthenticationEventArgs> PreAuthenticationEvent;
+
+        internal void InvokePreAuthenticationEvent(string userid, ref bool allow, ref string reason, ConnectionRequest request)
+        {
+            var ev = new PreAuthenticationEventArgs {Allow = allow, Request = request,UserId = userid, Reason = reason};
+            PreAuthenticationEvent?.Invoke(ev);
+
+            allow = ev.Allow;
+            reason = ev.Reason;
+        }
     }
 }
---
+++ Synapse/Patches/EventsPatches/ServerPatches/PreAuthenticationPatch.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using Harmony;
+using LiteNetLib;
+using LiteNetLib.Utils;
+
+namespace Synapse.Patches.EventsPatches.ServerPatches
+{
+    [HarmonyPatch(typeof(CustomLiteNetLib4MirrorTransport), nameof(CustomLiteNetLib4MirrorTransport.ProcessConnectionRequest))]
+    internal class PreAuthenticationPatch
+    {
+        private static void Postfix(CustomLiteNetLib4MirrorTransport __instance, ConnectionRequest request)
+        {
+            var allow = true;
+            var reason = "No Reason";
+
+            if (!request.Data.EndOfData)
+            {
+                return;
+            }
+
+            var userId = CustomLiteNetLib4MirrorTransport.UserIds[request.RemoteEndPoint].UserId;
+            SynapseController.Server.Events.Server.InvokePreAuthenticationEvent(userId, ref allow, ref reason, request);
+
+            if (allow)
+            {
+                request.Accept();
+                return;
+            }
+            
+            var data = new NetDataWriter();
+            data.Put((byte) 10);
+            data.Put(reason);
+            request.RejectForce(data);
+        }
+        
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            foreach (var code in codes.Select((x, i) => new {Value = x, Index = i}))
+            {
+                if (code.Value.opcode != OpCodes.Callvirt) continue;
+                if(codes[code.Index + 2].opcode != OpCodes.Ldstr) continue;
+
+                var strOperand = codes[code.Index + 2].operand as string;
+
+                if (strOperand == "Player {0} preauthenticated from endpoint {1}.")
+                {
+                    code.Value.opcode = OpCodes.Nop;
+                }
+            }
+
+            return codes.AsEnumerable();
+        }
+    }
+}
