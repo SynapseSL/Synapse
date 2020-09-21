4cb93170d6aa4b35bc37b5808c475a1f4832e8a0
Robin Möller <moelrobi@gmail.com>
2020-09-21 22:27:55 +0200

    + RoundStart + RoundCheck

--- Synapse/Api/Events/RoundEvents.cs
+++ Synapse/Api/Events/RoundEvents.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Synapse.Api.Events.SynapseEventArguments;
 
 namespace Synapse.Api.Events
 {
@@ -7,9 +8,28 @@ namespace Synapse.Api.Events
         internal RoundEvents() { }
 
         public event Action WaitingForPlayersEvent;
+        
+        public event Action RoundStartEvent;
+
+        public event EventHandler.OnSynapseEvent<RoundCheckEventArgs> RoundCheckEvent; 
 
         #region PlayerEventsInvoke
         internal void InvokeWaitingForPlayers() => WaitingForPlayersEvent?.Invoke();
+        internal void InvokeRoundStartEvent() => RoundStartEvent?.Invoke();
+
+        internal void InvokeRoundCheckEvent(ref bool forceEnd, ref bool allow, ref RoundSummary.LeadingTeam team,
+            ref bool teamChanged)
+        {
+            var ev = new RoundCheckEventArgs
+                {Allow = allow, Team = team, ForceEnd = forceEnd, TeamChanged = teamChanged};
+            RoundCheckEvent?.Invoke(ev);
+
+            allow = ev.Allow;
+            team = ev.Team;
+            forceEnd = ev.ForceEnd;
+            teamChanged = ev.TeamChanged;
+        }
+
         #endregion
     }
 }
---
+++ Synapse/Api/Events/SynapseEventArguments/RoundEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace Synapse.Api.Events.SynapseEventArguments
+{
+    public class RoundCheckEventArgs: EventHandler.ISynapseEventArgs
+    {
+        public bool ForceEnd { get; set; }
+        
+        public bool Allow { get; set; }
+        
+        public RoundSummary.LeadingTeam Team { get; set; }
+        
+        public bool TeamChanged { get; set; }
+    }
+}
