bffed07ef416cef224182cc3a85a635e7e00a3d1
Robin Möller <moelrobi@gmail.com>
2020-09-21 21:49:32 +0200

    + RemoteAdmin + ConsoleCommand

---
+++ Synapse/Patches/EventsPatches/ServerPatches/RemoteAdminPatch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Harmony;
+using RemoteAdmin;
+
+namespace Synapse.Patches.EventsPatches.ServerPatches
+{
+    [HarmonyPatch(typeof(CommandProcessor), nameof(CommandProcessor.ProcessQuery), typeof(string), typeof(CommandSender))]
+    internal static class RemoteAdminPatch
+    {
+        private static bool Prefix(ref string q, ref CommandSender sender)
+        {
+            try
+            {
+                if (q.Contains("REQUEST_DATA PLAYER_LIST SILENT")) return true;
+
+                var allow = true;
+                SynapseController.Server.Events.Server.InvokeRemoteAdminCommandEvent(sender, q, ref allow);
+
+                return allow;
+            }
+            catch (Exception e)
+            {
+                SynapseController.Server.Logger.Error($"Synapse-Event: RemoteAdminEvent failed!!\n{e}");
+                return true;
+            }
+        }
+    }
+}
