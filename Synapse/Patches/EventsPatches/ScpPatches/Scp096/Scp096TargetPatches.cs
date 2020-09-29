3392de5e44246d9a9b0de773524008f20fc4e94f
Robin Möller <moelrobi@gmail.com>
2020-09-29 14:36:12 +0200

    * Namespace Move

--- Synapse/Patches/EventsPatches/ScpPatches/Scp096/Scp096TargetPatches.cs
+++ Synapse/Patches/EventsPatches/ScpPatches/Scp096/Scp096TargetPatches.cs
@@ -3,12 +3,12 @@ using Harmony;
 using PlayableScps;
 using Synapse.Api;
 
-namespace Synapse.Events.Patches.EventPatches.ScpPatches
+namespace Synapse.Patches.EventsPatches.ScpPatches.Scp096
 {
-    [HarmonyPatch(typeof(Scp096), nameof(Scp096.ParseVisionInformation))]
+    [HarmonyPatch(typeof(PlayableScps.Scp096), nameof(PlayableScps.Scp096.ParseVisionInformation))]
     static class Scp096LookPatch
     {
-        public static bool Prefix(Scp096 __instance, VisionInformation info)
+        public static bool Prefix(PlayableScps.Scp096 __instance, VisionInformation info)
         {
             try
             {
@@ -17,16 +17,16 @@ namespace Synapse.Events.Patches.EventPatches.ScpPatches
             }
             catch (Exception e)
             {
-                Logger.Get.Info($"Synapse-Event: Scp096AddTargetEvent failed!!\n{e}");
+                Logger.Get.Error($"Synapse-Event: Scp096AddTargetEvent failed!!\n{e}");
                 return true;
             }
         }
     }
 
-    [HarmonyPatch(typeof(Scp096), nameof(Scp096.OnDamage))]
+    [HarmonyPatch(typeof(PlayableScps.Scp096), nameof(PlayableScps.Scp096.OnDamage))]
     static class Scp096ShootPatch
     {
-        public static bool Prefix(Scp096 __instance, PlayerStats.HitInfo info)
+        public static bool Prefix(PlayableScps.Scp096 __instance, PlayerStats.HitInfo info)
         {
             try
             {
@@ -35,7 +35,7 @@ namespace Synapse.Events.Patches.EventPatches.ScpPatches
             }
             catch (Exception e)
             {
-                Logger.Get.Info($"Synapse-Event: Scp096AddTargetEvent failed!!\n{e}");
+                Logger.Get.Error($"Synapse-Event: Scp096AddTargetEvent failed!!\n{e}");
                 return true;
             }
         }
