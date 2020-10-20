6add5b60e430af1dc472cf590f34cb0efc93a3ab
GrafDimenzio <crafterwolfhd@gmail.com>
2020-10-20 19:23:43 +0200

    fixed Nuke bug

--- Synapse/Patches/SynapsePatches/Wrapper/NukeLockPatch.cs
+++ Synapse/Patches/SynapsePatches/Wrapper/NukeLockPatch.cs
@@ -6,6 +6,6 @@ namespace Synapse.Patches.SynapsePatches
     [HarmonyPatch(typeof(PlayerInteract),nameof(PlayerInteract.CallCmdUsePanel))]
     internal static class NukeLockPatch
     {
-        private static bool Prefix() => Map.Get.Nuke.InsidePanel.Locked;
+        private static bool Prefix() => !Map.Get.Nuke.InsidePanel.Locked;
     }
 }
