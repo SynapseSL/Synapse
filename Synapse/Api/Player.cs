a58da8f4d226fa8c2b4e00571c0b26ae99b5d880
GrafDimenzio <crafterwolfhd@gmail.com>
2020-11-15 12:10:35 +0100

    fixed Player.Hurt()

--- Synapse/Api/Player.cs
+++ Synapse/Api/Player.cs
@@ -103,7 +103,7 @@ namespace Synapse.Api
         }
 
         public void Hurt(int amount, DamageTypes.DamageType damagetype = default, Player attacker = null) =>
-            PlayerStats.HurtPlayer(new PlayerStats.HitInfo(amount, attacker == null ? "WORLD" : attacker.NickName, damagetype, attacker == null ? PlayerId : attacker.PlayerId), attacker == null ? gameObject : attacker.gameObject);
+            PlayerStats.HurtPlayer(new PlayerStats.HitInfo(amount, attacker == null ? "WORLD" : attacker.NickName, damagetype, attacker == null ? 0 : attacker.PlayerId), attacker == null ? null : attacker.gameObject);
 
         public void SendToServer(ushort port)
         {
