5a953dd23dded944862fe0de87a3a94bfd796487
GrafDimenzio <crafterwolfhd@gmail.com>
2021-05-12 15:52:06 +0200

    fixed inheritance

--- Synapse/Permission/SynapseGroup.cs
+++ Synapse/Permission/SynapseGroup.cs
@@ -50,29 +50,28 @@ namespace Synapse.Permission
         {
             if(count > 50) return false;
 
-            if (permission == null || Permissions == null) return false;
+            if (permission != null && Permissions != null)
+                foreach (var perm in Permissions)
+                {
+                    if (perm == "*" || perm == "*.*" || perm == ".*") return true;
 
-            foreach (var perm in Permissions)
-            {
-                if (perm == "*" || perm == "*.*" || perm == ".*") return true;
+                    if (permission.ToUpper() == perm.ToUpper()) return true;
 
-                if (permission.ToUpper() == perm.ToUpper()) return true;
+                    var args = permission.Split('.');
+                    var args2 = perm.Split('.');
 
-                var args = permission.Split('.');
-                var args2 = perm.Split('.');
+                    if (args.Length == 1 || args2.Length == 1) continue;
 
-                if (args.Length == 1 || args2.Length == 1) continue;
+                    if (args2[0].ToUpper() == args[0].ToUpper())
+                        for (int i = 1; i < args.Length; i++)
+                        {
+                            if (args.Length < i + 1 || args2.Length < i + 1) break;
 
-                if (args2[0].ToUpper() == args[0].ToUpper())
-                    for (int i = 1; i < args.Length; i++)
-                    {
-                        if (args.Length < i + 1 || args2.Length < i + 1) break;
+                            if (args2[i] == "*") return true;
 
-                        if (args2[i] == "*") return true;
-
-                        if (args[i].ToUpper() != args2[i].ToUpper()) break;
-                    }
-            }
+                            if (args[i].ToUpper() != args2[i].ToUpper()) break;
+                        }
+                }
 
             if (Inheritance == null) return false;
 
