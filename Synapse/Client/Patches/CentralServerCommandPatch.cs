f772a8329cd07849f83b16b8e6c935e7371403b6
GrafDimenzio <crafterwolfhd@gmail.com>
2021-06-06 15:36:12 +0200

    disabled CentralServerCommands

---
+++ Synapse/Client/Patches/CentralServerCommandPatch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HarmonyLib;
+
+namespace Synapse.Client.Patches
+{
+    [HarmonyPatch(typeof(ServerConsole),nameof(ServerConsole.RunCentralServerCommand))]
+    internal static class CentralServerCommandPatch
+    {
+        private static bool Prefix()
+        {
+            if (!ClientManager.isSynapseClientEnabled) return true;
+            Synapse.Api.Logger.Get.Info("The Synapse Central Server does not support Central Server Commands");
+            return false;
+        }
+    }
+}
