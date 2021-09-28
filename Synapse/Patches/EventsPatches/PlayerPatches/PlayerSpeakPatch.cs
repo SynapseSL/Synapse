62bb671d132c1d05d159066ae161282adea58ac7
AlmightyLks <AlmightyLks@protonmail.com>
2021-09-28 20:09:12 +0200

    Progress

--- Synapse/Patches/EventsPatches/PlayerPatches/PlayerSpeakPatch.cs
+++ Synapse/Patches/EventsPatches/PlayerPatches/PlayerSpeakPatch.cs
@@ -5,12 +5,13 @@ using Synapse.Api.Events.SynapseEventArguments;
 
 namespace Synapse.Patches.EventsPatches.PlayerPatches
 {
-    // Setters are also being called when the player is - for instance - not an SCP, yet it sets SCPChat to "false" for no reason
-    // Setting it to
-
-    [HarmonyPatch(typeof(DissonanceUserSetup), nameof(DissonanceUserSetup.SCPChat), MethodType.Setter)]
     internal static class PlayerSpeakPatch
     {
+        [HarmonyPatch(typeof(Radio))]
+        internal static class RadioPatch
+        {
+
+        }
         [HarmonyPrefix]
         [HarmonyPatch(nameof(DissonanceUserSetup.SCPChat), MethodType.Setter)]
         private static bool SCPChatSetter(DissonanceUserSetup __instance, ref bool value)
@@ -22,45 +23,7 @@ namespace Synapse.Patches.EventsPatches.PlayerPatches
             }
             return true;
         }
-        /*
-        [HarmonyPrefix]
-        [HarmonyPatch(nameof(DissonanceUserSetup.SCPChat), MethodType.Setter)]
-        private static bool SCPChatSetter(DissonanceUserSetup __instance, ref bool value)
-        {
-            var player = __instance.GetPlayer();
-            if (player.Team == Team.SCP)
-            {
-                Synapse.Api.Logger.Get.Debug("VC as SCP");
-            }
-            return true;
-        }
-        */
 
-        [HarmonyPrefix]
-        [HarmonyPatch(nameof(DissonanceUserSetup.RadioAsHuman), MethodType.Setter)]
-        private static bool RadioAsHumanSetter(DissonanceUserSetup __instance, ref bool value)
-        {
-            var player = __instance.GetPlayer();
-            if (player.Team != Team.SCP && player.Team != Team.RIP) //Human = not scp & not spectator
-            {
-                Synapse.Api.Logger.Get.Debug("VC as Human");
-            }
-            return true;
-        }
-
-        [HarmonyPrefix]
-        [HarmonyPatch(nameof(DissonanceUserSetup.SpectatorChat), MethodType.Setter)]
-        private static bool SpectatorChatSetter(DissonanceUserSetup __instance, ref bool value)
-        {
-            Synapse.Api.Logger.Get.Debug(value);
-            var player = __instance.GetPlayer();
-            if (player.Team == Team.RIP)
-            {
-                Synapse.Api.Logger.Get.Debug("VC as Human");
-            }
-            return true;
-        }
-        
         /*
         private static void InvokeEvent(DissonanceUserSetup __instance)
         {
@@ -114,5 +77,62 @@ namespace Synapse.Patches.EventsPatches.PlayerPatches
             }
         }
     */
+    }
+
+    //TODO: Rework
+    [HarmonyPatch(typeof(Radio), nameof(Radio.UserCode_CmdSyncTransmissionStatus))]
+    internal static class PlayerSpeakPatch1
+    {
+        [HarmonyPrefix]
+        private static bool AltIsActive(Radio __instance, bool b)
+        {
+            try
+            {
+                var intercom = __instance._dissonanceSetup.IntercomAsHuman;
+                var radio = __instance._dissonanceSetup.RadioAsHuman;
+                var scp939 = Server.Get.Configs.synapseConfiguration.SpeakingScps.Contains(__instance.GetPlayer().RoleID);
+                var scpChat = __instance._dissonanceSetup.SCPChat;
+                var specChat = __instance._dissonanceSetup.SpectatorChat;
+                var allow = true;
+
+                SynapseController.Server.Events.Player.InvokePlayerSpeakEvent(__instance._dissonanceSetup, ref intercom, ref radio, ref scp939, ref scpChat, ref specChat, ref allow);
+
+                __instance._dissonanceSetup.SCPChat = scpChat;
+                __instance._dissonanceSetup.SpectatorChat = specChat;
+                __instance._dissonanceSetup.IntercomAsHuman = intercom;
+
+                if (scp939) __instance._dissonanceSetup.MimicAs939 = b;
+                else __instance._dissonanceSetup.MimicAs939 = false;
+
+                if (radio) __instance._dissonanceSetup.RadioAsHuman = b;
+
+                try
+                {
+                    if (__instance._dissonanceSetup.RadioAsHuman)
+                    {
+                        var player = __instance.GetPlayer();
+                        var item = __instance.RadioItem;
+
+                        if (item != null)
+                        {
+                            var allowradio = true;
+                            SynapseController.Server.Events.Player.InvokePlayerItemUseEvent(player, item.GetSynapseItem(), ItemInteractState.Finalizing, ref allowradio);
+                            __instance._dissonanceSetup.RadioAsHuman = allowradio;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    SynapseController.Server.Logger.Error($"Synapse-Event: PlayerUseItemEvent(Radio) failed!!\n{e}");
+                }
+
+                return allow;
+            }
+            catch (Exception e)
+            {
+                SynapseController.Server.Logger.Error($"Synapse-Event: PlayerSpeak failed!!\n{e}");
+                return true;
+            }
+        }
     }
 }
