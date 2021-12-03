f542a173907ee158066e4c719ccd4669e0496235
Polyfish0 <westphal.mika@freenet.de>
2021-12-03 18:40:27 +0100

    First patch for Ragdoll ; Missing Recall functionality

--- Synapse/Api/Ragdoll.cs
+++ Synapse/Api/Ragdoll.cs
@@ -1,6 +1,7 @@
 ﻿using Mirror;
 using UnityEngine;
 using System.Linq;
+using PlayerStatsSystem;
 
 namespace Synapse.Api
 {
@@ -8,7 +9,7 @@ namespace Synapse.Api
     {
         internal Ragdoll(global::Ragdoll rag) => ragdoll = rag;
 
-        public Ragdoll(RoleType roletype, Vector3 pos, Quaternion rot, Vector3 velocity, PlayerStats.HitInfo info, bool allowRecall, string owner)
+        /*public Ragdoll(RoleType roletype, Vector3 pos, Quaternion rot, Vector3 velocity, PlayerStats.HitInfo info, bool allowRecall, string owner)
         {
             var role = Server.Get.Host.ClassManager.Classes.SafeGet((int)roletype);
             var gameobject = UnityEngine.Object.Instantiate(role.model_ragdoll, pos + role.ragdoll_offset.position, Quaternion.Euler(rot.eulerAngles + role.ragdoll_offset.rotation));
@@ -18,6 +19,16 @@ namespace Synapse.Api
             ragdoll.NetworkallowRecall = allowRecall;
             ragdoll.NetworkPlayerVelo = velocity;
             Map.Get.Ragdolls.Add(this);
+        }*/
+
+        public Ragdoll(RoleType roletype, Vector3 pos, Quaternion rot, DamageHandlerBase handler, Player owner)
+        {
+            Role role = Server.Get.Host.ClassManager.Classes.SafeGet((int) roletype);
+            GameObject gameObject = Object.Instantiate(role.model_ragdoll, pos + role.model_offset.position,
+                Quaternion.Euler(rot.eulerAngles + role.model_offset.rotation));
+            NetworkServer.Spawn(gameObject);
+            ragdoll = gameObject.GetComponent<global::Ragdoll>();
+            ragdoll.NetworkInfo = new RagdollInfo(owner.Hub, handler, pos, rot);
         }
 
         private readonly global::Ragdoll ragdoll;
@@ -28,7 +39,7 @@ namespace Synapse.Api
 
         public RoleType RoleType
         {
-            get => Server.Get.Host.ClassManager.Classes.FirstOrDefault(role => role.fullName == ragdoll.owner.FullName).roleId;
+            get => ragdoll.Info.RoleType;
         }
 
         public Vector3 Position
@@ -55,19 +66,7 @@ namespace Synapse.Api
 
         public Player Owner
         {
-            get => Server.Get.GetPlayer(ragdoll.owner.PlayerId);
-            set
-            {
-                ragdoll.owner.PlayerId = value.PlayerId;
-                ragdoll.owner.Nick = value.NickName;
-                ragdoll.owner.ownerHLAPI_id = value.GetComponent<Dissonance.Integrations.MirrorIgnorance.MirrorIgnorancePlayer>().PlayerId;
-            }
-        }
-
-        public bool AllowRecall
-        {
-            get => ragdoll.allowRecall;
-            set => ragdoll.allowRecall = value;
+            get => Server.Get.GetPlayer(ragdoll.Info.OwnerHub.playerId);
         }
 
         public void Destroy()
@@ -76,7 +75,7 @@ namespace Synapse.Api
             Map.Get.Ragdolls.Remove(this);
         }
         
-        public static Ragdoll CreateRagdoll(RoleType roletype, Vector3 pos, Quaternion rot, Vector3 velocity, PlayerStats.HitInfo info, bool allowRecall, Player owner) 
-            => new Ragdoll(roletype, pos, rot, velocity, info, allowRecall, owner.NickName);
+        public static Ragdoll CreateRagdoll(RoleType roletype, Vector3 pos, Quaternion rot, DamageHandlerBase handler, Player owner) 
+            => new Ragdoll(roletype, pos, rot, handler, owner);
     }
 }
