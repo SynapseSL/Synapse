8bffe04a8ed7ceb3c2277f7b4651ec8a1fb62cf6
Bonjemus <59416055+Bonjemus@users.noreply.github.com>
2022-05-09 14:45:51 +0200

    +BreackNeckPatch

---
+++ Synapse/Patches/EventsPatches/ScpPatches/Scp173/Scp173BreackNeckPatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using HarmonyLib;
+
+namespace Synapse.Patches.EventsPatches.ScpPatches.Scp173
+{
+    [HarmonyPatch(typeof(PlayableScps.Scp173), nameof(PlayableScps.Scp173.ServerDoBreakneckSpeeds))]
+    internal static class Scp173BreackNeckPatch
+    {
+        [HarmonyPrefix]
+        private static bool ServerScp173BreackNeckPatch(PlayableScps.Scp173 __instance)
+        {
+            try
+            {
+                SynapseController.Server.Events.Scp.Scp173.InvokeScp173BreakNeckEvent(__instance.GetPlayer(), out bool allow);
+                return allow;
+            }
+            catch(Exception e)
+            {
+                Api.Logger.Get.Error($"Synapse-Event: Scp173BreackNeckPatch(Scp173) failed!!\n{e}");
+                return true;
+            }
+        }
+    }
+}
