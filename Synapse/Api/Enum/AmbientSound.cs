30fec2039a2a5cecea69f1bf8bacfbdc91888d7e
Valentin Arthur Thomas <64769541+warquys@users.noreply.github.com>
2022-05-12 17:17:46 +0200

    AmbientSound & CassieSubtile & Voltage (#106)
    AmbientSound & CassieSubtile

---
+++ Synapse/Api/Enum/AmbientSound.cs
@@ -0,0 +1,38 @@
+﻿namespace Synapse.Api.Enum
+{
+    public enum AmbientSound
+    {
+        PowerEngaged = 0,
+        HeavyObjectFall = 1,
+        Tile = 2,
+        HeavyDoor = 3,
+        LightSlipingObject = 4,
+        Whale = 5,
+        Scp079 = 6,
+        InformaticBeep = 7,
+        Circle = 8,
+        RisingCiment = 9,
+        Breath = 10,
+        BigBreath = 11,
+        HeavyBreath = 12,
+        SmollBreath = 13,
+        LitleBreath = 14,
+        LongBreath = 15,
+        LightWaher = 16,
+        Crash = 17,
+        MechanicalMovement = 18,
+        Wind = 19,
+        SlipingObject = 20,
+        MultipleSmallBeep = 21,
+        LongBeep = 22,
+        AlertBeep = 23,
+        AudibleLongBeep = 24,
+        RisingAndFallingBeep = 25,
+        WeakBeep = 26,
+        Valve = 27,
+        Thunderstorm = 28,
+        OldHardDrive = 29,
+        AudibleAlertBeep = 30,
+        ComputerSound = 31,
+    }
+}
--- Synapse/Api/Map.cs
+++ Synapse/Api/Map.cs
@@ -90,6 +90,19 @@ namespace Synapse.Api
 
         public int Seed => MapGeneration.SeedSynchronizer.Seed;
 
+        public void PlayAmbientSound(AmbientSound id)
+            => Server.Get.Host.GetComponent<AmbientSoundPlayer>().RpcPlaySound((int)id);
+
+
+        /// <returns>1 activated <see cref="Generator"/> gives 1000 volts</returns>
+        public int GetVoltage()
+        {
+            float totalvoltagefloat = 0;
+            foreach (var generator in Generators)
+                totalvoltagefloat += generator.generator._currentTime / generator.generator._totalActivationTime * 1000;
+            return (int)totalvoltagefloat;
+        }
+
         public Room GetRoom(RoomName roomType)
             => Rooms.FirstOrDefault(x => x.RoomType == roomType);
 
@@ -122,8 +135,11 @@ namespace Synapse.Api
         });
 
         public void Cassie(string words, bool makehold = true, bool makenoise = true)
-            => Respawning.RespawnEffectsController.PlayCassieAnnouncement(words, makehold, makenoise);
+            => Respawning.RespawnEffectsController.PlayCassieAnnouncement(words, makehold, makenoise, false);
 
+        public void SubtitleCassie(string words, bool makehold = true, bool makenoise = true)
+            => Respawning.RespawnEffectsController.PlayCassieAnnouncement(words, makehold, makenoise, true);
+        
         public void GlitchedCassie(string words)
         {
             float num2 = (AlphaWarheadController.Host.timeToDetonation <= 0f) ? 3.5f : 1f;
