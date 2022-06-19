7333c8aec4dd0537d5b36bef52c4dc60d3a4257d
Helight <helightdev@gmail.com>
2022-06-19 15:23:44 +0200

    Example Module

---
+++ Synapse3.ExampleModule/ExampleCommand.cs
@@ -0,0 +1,32 @@
+﻿using Neuron.Core.Dev;
+using Neuron.Core.Meta;
+using Neuron.Modules.Commands;
+using Ninject;
+using Synapse3.ExampleModule;
+using Synapse3.SynapseModule.Command;
+
+namespace Synapse3.ExamplePlugin;
+
+[Automatic]
+[SynapseCommand(
+    CommandName = "Example2",
+    Description = "An example command",
+    Aliases = new []{"Ex2"},
+    Platforms = new [] {CommandPlatform.RemoteAdmin, CommandPlatform.PlayerConsole, CommandPlatform.ServerConsole}
+)]
+public class ExampleCommand : SynapseCommand
+{
+    
+    [Inject]
+    public ExampleConfig Config { get; set; }
+    
+    [Inject]
+    public ExampleTranslations Translations { get; set; }
+    
+    public override void Execute(SynapseContext context, ref CommandResult result)
+    {
+        Logger.Fatal(Config);
+        Logger.Fatal(Translations);
+        result.Response = Translations.CommandMessage.Format(Config.StringEntry, "Günter");
+    }
+}
---
+++ Synapse3.ExampleModule/ExampleModule.cs
@@ -0,0 +1,43 @@
+﻿using Neuron.Core.Dev;
+using Neuron.Core.Modules;
+using Ninject;
+using Synapse3.ExamplePlugin;
+
+namespace Synapse3.ExampleModule;
+
+[Module(
+    Name = "Example Module",
+    Description = "Example Description",
+    Dependencies = new []
+    {
+        typeof(SynapseModule.SynapseModule)
+    }
+)]
+public class ExampleModule : Module
+{
+    [Inject]
+    public ExampleConfig Config { get; set; }
+        
+    [Inject]
+    public ExampleTranslations Translations { get; set; }
+
+    public override void Load(IKernel kernel)
+    {
+        Logger.Info($"Before {Config}");
+        Logger.Info($"Before {Translations}");
+    }
+
+    public override void Enable()
+    {
+        Logger.Info(Config.StringEntry);
+        Logger.Info(Config.IntEntry);
+        Logger.Info(Config.ListEntry);
+        Logger.Info(Translations.CommandMessage.Format("Example Command", "Helight"));
+
+    }
+
+    public override void Disable()
+    {
+            
+    }
+}
