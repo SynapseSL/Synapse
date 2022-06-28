e0c433a6a06b201adb44c210f1a76d74952fd7e8
Flo0205 <florian0205@gmx.de>
2022-06-28 17:51:35 +0200

    added DecontaminationService

---
+++ Synapse3.SynapseModule/Map/DecontaminationService.cs
@@ -0,0 +1,31 @@
+﻿using LightContainmentZoneDecontamination;
+using Neuron.Core.Meta;
+
+namespace Synapse3.SynapseModule.Map;
+
+public class DecontaminationService : Service
+{
+    /// <summary>
+    /// Returns the Decontamination Controller
+    /// </summary>
+    public DecontaminationController Controller => DecontaminationController.Singleton;
+
+    /// <summary>
+    /// Lock the decontamination progress in its current phase
+    /// </summary>
+    public bool Locked
+    {
+        get => Controller._stopUpdating; 
+        set => Controller._stopUpdating = value;
+    }
+
+    /// <summary>
+    /// Returns whether the Decontamination is in Progress
+    /// </summary>
+    public bool IsDecontaminationInProgress => Controller._decontaminationBegun;
+
+    /// <summary>
+    /// Starts the Decontamination
+    /// </summary>
+    public void InstantStart() => Controller.FinishDecontamination();
+}
