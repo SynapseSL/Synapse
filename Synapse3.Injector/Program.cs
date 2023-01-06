9a68f1b3892a13084fbf002e28c5b3a8bb0db5eb
warquys <64769541+warquys@users.noreply.github.com>
2023-01-06 21:26:51 +0100

    Remove debug

--- Synapse3.Injector/Program.cs
+++ Synapse3.Injector/Program.cs
@@ -11,8 +11,6 @@ namespace Synapse3.Injector
             var injector = new SynapseInjector();
             try
             {
-                args = new string[1] { "E:\\Games\\SteamLibrary\\steamapps\\common\\SCP Secret Laboratory Dedicated Server\\SCPSL_Data\\Managed\\Assembly-CSharp.dll" };
-
                 if (args.Length == 0)
                     return;
 
