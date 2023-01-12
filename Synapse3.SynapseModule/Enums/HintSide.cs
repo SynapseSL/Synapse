589ce93516b564e57d877b5eb27fc3d063e44489
warquys <64769541+warquys@users.noreply.github.com>
2023-01-12 22:19:15 +0100

    NameSpace

--- Synapse3.SynapseModule/Enums/HintSide.cs
+++ Synapse3.SynapseModule/Enums/HintSide.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Synapse3.SynapseModule.Enums;
 
-namespace Synapse3.SynapseModule.Enums
+public enum HintSide
 {
-    public enum HintSide
-    {
-        Left, 
-        Right,
-        /// <summary>
-        /// Display on the both side Left anr Right
-        /// </summary>
-        Midle,
-    }
+    Left, 
+    Right,
+    /// <summary>
+    /// Display on the both side Left anr Right
+    /// </summary>
+    Midle,
 }
