061c813a1b74f6fa4b85869d564a834fcacdec62
warquys <64769541+warquys@users.noreply.github.com>
2023-02-13 23:38:38 +0100

    Add interface property

--- Synapse3.SynapseModule/Map/Rooms/VanillaRoom.cs
+++ Synapse3.SynapseModule/Map/Rooms/VanillaRoom.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using MapGeneration;
 using Synapse3.SynapseModule.Map.Objects;
 using UnityEngine;
 
@@ -8,5 +9,11 @@ public interface IVanillaRoom : IRoom
 {
     public ReadOnlyCollection<SynapseCamera> Cameras { get; }
 
+    public RoomIdentifier Identifier { get; }
+
+    public FlickerableLightController LightController { get; }
+
     public Color WarheadColor { get; set; }
+
+    public RoomType RoomType { get; }
 }
